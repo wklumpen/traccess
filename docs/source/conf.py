3edf1b850b645fc46e88458af32b991b81818368
Willem Klumpenhouwer <willemjjk@gmail.com>
2024-02-08 14:16:11 -0700

    RDT

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
 project = "traccess"
-copyright = "2023, Willem Klumpenhouwer"
+copyright = "2024, Willem Klumpenhouwer"
 author = "Willem Klumpenhouwer"
 
 # -- General configuration ---------------------------------------------------
