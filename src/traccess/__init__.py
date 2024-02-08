486cba78b6df207dd74f86fc5ad34b83ca511056
Willem Klumpenhouwer <willemjjk@gmail.com>
2024-02-08 16:11:38 -0700

    Increment version number

--- src/traccess/__init__.py
+++ src/traccess/__init__.py
@@ -1,6 +1,6 @@
 "Python library for computing access to opportunities and equity metrics"
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .data import Cost, Demographic, Supply, Access
 from .access import AccessComputer, EquityComputer
